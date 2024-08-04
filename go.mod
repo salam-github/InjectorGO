6c21763f32b3426a062a16c88c3639713825ff9a
salam <salamfoon@gmail.com>
2024-08-05 01:33:56 +0300

    chore: Update go.mod and go.sum files, and remove unused PYTHON.md file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module test
 
 go 1.18
+
+require fyne.io/fyne/v2 v2.5.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	_ "embed"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,117 +8,145 @@ import (
 	"path/filepath"
 )
 
-//go:embed important1.exe
 var embeddedProgram1 []byte
-
-//go:embed important2.exe
 var embeddedProgram2 []byte
 
 func main() {
-	var file1, file2 string
-	// Check if any command-line arguments were provided
 	if filepath.Base(os.Args[0]) == "injector.exe" {
-		var err error
-		// Check command line arguments
-		file1, file2, err = check_args()
+		if len(os.Args) == 3 {
+			file1, file2, err := checkArgs()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			err = embedAndCreateExecutable(file1, file2)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println("Successfully created newprogram.exe.")
+		} else {
+			fmt.Printf("Usage: %s <file1> <file2>\n", os.Args[0])
+			os.Exit(1)
+		}
+	} else {
+		// When running newprogram.exe
+		embeddedProgram1, err := os.ReadFile("important1.exe")
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		// Read the contents of the provided files
-		f1, err := os.ReadFile(file1)
+		embeddedProgram2, err := os.ReadFile("important2.exe")
 		if err != nil {
-			panic(err)
+			fmt.Println(err)
+			os.Exit(1)
 		}
-		f2, err := os.ReadFile(file2)
-		if err != nil {
-			panic(err)
-		}
-		embeddedProgram1 = []byte(f1)
-		embeddedProgram2 = []byte(f2)
-		// Write the new contents to important1.exe and important2.exe
-		os.WriteFile("important1.exe", f1, 0755)
-		os.WriteFile("important2.exe", f2, 0755)
-
-		// Run `go build` as a subprocess
-		cmd := exec.Command("go", "build", "-o", "newprogram.exe", "main.go")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err = cmd.Run()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Successfully injected the embedded executables into newprogram.exe")
-		return
-	}
-
-	// Write the embedded executables to temporary files
-	tmpfile1, err := ioutil.TempFile("", "embedded_program1_*.exe")
-	if err != nil {
-		panic(err)
-	}
-	defer os.Remove(tmpfile1.Name()) // Clean up the temporary file
-
-	_, err = tmpfile1.Write(embeddedProgram1)
-	if err != nil {
-		panic(err)
-	}
-	err = tmpfile1.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	tmpfile2, err := ioutil.TempFile("", "embedded_program2_*.exe")
-	if err != nil {
-		panic(err)
-	}
-	defer os.Remove(tmpfile2.Name()) // Clean up the temporary file
-
-	_, err = tmpfile2.Write(embeddedProgram2)
-	if err != nil {
-		panic(err)
-	}
-	err = tmpfile2.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	// Make the temporary files executable
-	err = os.Chmod(tmpfile1.Name(), 0755)
-	if err != nil {
-		panic(err)
-	}
-
-	err = os.Chmod(tmpfile2.Name(), 0755)
-	if err != nil {
-		panic(err)
-	}
-
-	if filepath.Base(os.Args[0]) != "injector.exe" {
-		// Execute the embedded executables
-		cmd1 := exec.Command(tmpfile1.Name())
-		cmd1.Stdout = os.Stdout
-		cmd1.Stderr = os.Stderr
-		err = cmd1.Run()
-		if err != nil {
-			panic(err)
-		}
-
-		cmd2 := exec.Command(tmpfile2.Name())
-		cmd2.Stdout = os.Stdout
-		cmd2.Stderr = os.Stderr
-		err = cmd2.Run()
-		if err != nil {
-			panic(err)
-		}
+		writeAndExecuteTemporaryFiles(embeddedProgram1, embeddedProgram2)
 	}
 }
 
-// return 2 arguments from command line or error
-func check_args() (string, string, error) {
+func checkArgs() (string, string, error) {
 	args := os.Args[1:]
 	if len(args) != 2 {
 		return "", "", fmt.Errorf("Usage: %s <file1> <file2>", os.Args[0])
 	}
 	return args[0], args[1], nil
+}
+
+func embedAndCreateExecutable(file1, file2 string) error {
+	f1, err := os.ReadFile(file1)
+	if err != nil {
+		return err
+	}
+	f2, err := os.ReadFile(file2)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("important1.exe", f1, 0755)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile("important2.exe", f2, 0755)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("go", "build", "-o", "newprogram.exe", "main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeAndExecuteTemporaryFiles(embeddedProgram1, embeddedProgram2 []byte) {
+	tmpfile1, err := ioutil.TempFile("", "embedded_program1_*.exe")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer os.Remove(tmpfile1.Name())
+
+	_, err = tmpfile1.Write(embeddedProgram1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = tmpfile1.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	tmpfile2, err := ioutil.TempFile("", "embedded_program2_*.exe")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer os.Remove(tmpfile2.Name())
+
+	_, err = tmpfile2.Write(embeddedProgram2)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = tmpfile2.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = os.Chmod(tmpfile1.Name(), 0755)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = os.Chmod(tmpfile2.Name(), 0755)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	cmd1 := exec.Command(tmpfile1.Name())
+	cmd1.Stdout = os.Stdout
+	cmd1.Stderr = os.Stderr
+	err = cmd1.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	cmd2 := exec.Command(tmpfile2.Name())
+	cmd2.Stdout = os.Stdout
+	cmd2.Stderr = os.Stderr
+	err = cmd2.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
